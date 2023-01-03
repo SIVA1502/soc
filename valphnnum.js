7db1f0ba2b24f1135ddaa86c5df55b5fda4596cd
SIVA1502 <113298358+SIVA1502@users.noreply.github.com>
2023-01-03 07:47:55 +0530

    Create valphnnum.js

---
+++ valphnnum.js
@@ -0,0 +1,48 @@
+//XXXXXXXXXX
+function phonenumber(inputtxt)
+{
+  var phoneno = /^\d{10}$/;
+  if((inputtxt.value.match(phoneno))
+        {
+      return true;
+        }
+      else
+        {
+        alert("message");
+        return false;
+        }
+}
+
+
+
+//XXX.XXX.XXXX
+function phonenumber(inputtxt)
+{
+  var phoneno = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;
+  if((inputtxt.value.match(phoneno))
+        {
+      return true;
+        }
+      else
+        {
+        alert("message");
+        return false;
+        }
+}
+
+
+
+//+XX.XXXX.XXXX
+function phonenumber(inputtxt)
+{
+  var phoneno = /^\+?([0-9]{2})\)?[-. ]?([0-9]{4})[-. ]?([0-9]{4})$/;
+  if((inputtxt.value.match(phoneno))
+        {
+      return true;
+        }
+      else
+        {
+        alert("message");
+        return false;
+        }
+}
